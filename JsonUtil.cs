2ea5f6ed00d559f45a457f02ed247ba129a888b3
Voltucit <2804603767@qq.com>
2025-05-19 21:10:28 +0800

    添加用户配置文件保存逻辑

---
+++ JsonUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+
+namespace 忘却的旋律_EP;
+
+public static class JsonUtil
+{
+    private static readonly string FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LauncherSetting.json");
+
+    public static LauncherSettings Load()
+    {
+        if (!File.Exists(FilePath))
+        {
+            // 如果文件不存在，则返回默认值
+            return new LauncherSettings();
+        }
+
+        var json = File.ReadAllText(FilePath);
+        
+        
+        return JsonConvert.DeserializeObject<LauncherSettings>(json) ?? new LauncherSettings();
+    }
+
+    public static void Save(LauncherSettings settings)
+    {
+        var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+        File.WriteAllText(FilePath, json);
+    }
+}
---
+++ LauncherSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace 忘却的旋律_EP;
+
+public class LauncherSettings
+{
+    public int LoginMode { get; set; } = 1;
+    public string Playername { get; set; } = "";
+    public string GameVersion { get; set; } = ""; 
+    public string JavaPath { get; set; } = "";
+}
